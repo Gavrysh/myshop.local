3139e38f6119253af67201485adfe45b9cb3bb7e
GaSeS <very.nice@ukr.net>
2018-09-21 23:19:46 +0300

    Робота над меню сайту

--- models/CategoriesModel.php
+++ models/CategoriesModel.php
@@ -3,6 +3,20 @@
  * Модель для роботи з таблицею категорії (categories)
  */
 
+function getChildrenForCat($catId)
+{
+    $sql = "SELECT * FROM categories WHERE parent_id = '{$catId}'";
+
+    $rs = mysqli_query($GLOBALS['db'], $sql);
+
+    return createSmartyRsArray($rs);
+}
+
+/**
+ * Отримати головні категорії з прив'язками дочірніх
+ *
+ * @return array - масив категорій
+ */
 function getAllMainCatsWithChildren()
 {
     $sql = 'SELECT * FROM categories WHERE parent_id = 0';
@@ -10,7 +24,14 @@ function getAllMainCatsWithChildren()
     $rs = mysqli_query($GLOBALS['db'], $sql);
 
     $smartyRs = [];
+
     while ($row = mysqli_fetch_assoc($rs)) {
+        $rsChildren = getChildrenForCat($row['id']);
+
+        if ($rsChildren) {
+            $row['children'] = $rsChildren;
+        }
+
         $smartyRs[] = $row;
     }
 
